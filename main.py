117964fe70cb19172aecf232bf5dbb376574a048
Sebastin Justeeson <justeeson.1@osu.edu>
2022-02-28 19:21:14 -0500

    Changed a lot of function names to more accurately represent the new project. Also added a new button and changed the program logic so it can work with two images.

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import PIL
 import matplotlib.pyplot as plt
 from tkinter import *
 from tkinter.filedialog import askopenfilename
+import sys
 
-WINDOW_SIZE = '300x100'
-image_file = PIL.Image.new('RGB', (0, 0))
+# Our 2 image file objects
+main_image_file = PIL.Image.new('RGB', (0, 0))
+second_image_file = PIL.Image.new('RGB', (0, 0))
+
+# Size of the window for our program
+WINDOW_SIZE = '300x150'
 
 
-def generate_histogram(root):
-    """Function to generate the histogram for our image"""
-    global image_file
+def steganographize(root):
+    """Function to generate the new steganographized image"""
     image_file = get_image_file()
     width, height = grab_dimensions(image_file)
     R_array, G_array, B_array = initialize_color_arrays()
@@ -22,22 +26,30 @@ def generate_histogram(root):
     display_plots()
 
 
-def set_image_file(image):
+def set_image_file(image, image_file):
     """This function sets the image object to the right value"""
-    global image_file
-    image_file = PIL.Image.open(image)
+    global main_image_file
+    global second_image_file
+
+    # Set the right image file object based on which button was pressed
+    if image_file == "main":
+        main_image_file = PIL.Image.open(image)
+    elif image_file == "secondary":
+        second_image_file = PIL.Image.open(image)
+    else:
+        print("Something unexpected happened!")
+        sys.exit()
 
 
-def get_image_file():
-    """This function simply returns the global image variable"""
-    global image_file
+def get_image_file(image_file):
+    """This function simply returns the image variable"""
     return image_file
 
 
-def open_image(root):
+def open_image(root, image_file):
     """This function opens the image file and calls the set_image_file function"""
     root.withdraw()
-    set_image_file(askopenfilename(filetypes=(("Image Files", "*.jpg *.jpeg *.png"),)))
+    set_image_file(askopenfilename(filetypes=(("Image Files", "*.jpg *.jpeg *.png"),)), image_file)
     root.deiconify()
 
 
@@ -91,28 +103,39 @@ def create_buckets(image, width, height, R_array, G_array, B_array):
 
 def generate_ui():
     """Function to generate our GUI using tkinter"""
+    global main_image_file
+    global second_image_file
+
     root = Tk()
 
     root.geometry(WINDOW_SIZE)
 
-    frame1 = Frame(root)  # Image Select button
-    frame2 = Frame(root)  # Generate histogram button
+    # The frame variables for our 3 buttons
+    frame1, frame2, frame3 = Frame(root), Frame(root), Frame(root)
 
-    root.title("Image Histogram Viewer")
+    root.title("Image Steganographer")
 
-    # Button that executes the function to load the log file(s)
+    # Button to select our main image
     button_default_text = StringVar()
-    button_default_text.set("Select Image")
-    x = 5
-    select_button = Button(frame1, textvariable=button_default_text, command=lambda: open_image(root), width=22)
-    select_button.pack()
+    button_default_text.set("Select main image")
+    select_button_main = Button(frame1, textvariable=button_default_text, command=lambda: open_image(root, "main"),
+                                width=22)
+    select_button_main.pack()
+
+    # Button to select the image that we want to hide
+    button_default_text = StringVar()
+    button_default_text.set("Select image to hide")
+    select_button_secondary = Button(frame2, textvariable=button_default_text,
+                                     command=lambda: open_image(root, "secondary"), width=22)
+    select_button_secondary.pack()
 
     # Button that calls the conversion function
-    convert_button = Button(frame2, text="Generate Histogram", command=lambda: generate_histogram(root), width=22)
+    convert_button = Button(frame3, text="Steganographize", command=lambda: steganographize(root), width=22)
     convert_button.pack()
 
     frame1.pack(padx=10, pady=10)
     frame2.pack(padx=1, pady=1)
+    frame3.pack(padx=10, pady=10)
     root.mainloop()
 
 
